7f04112362bc7d84d1ec9e79b2d4982783bc8450
EthanMorinUC <morinej@mail.uc.edu>
2024-11-25 00:00:32 -0500

    Initial commit

---
+++ src/TicTacToe.java
@@ -0,0 +1,7 @@
+public class TicTacToe {
+    public static void TicTacToe(String[] args) {
+        // int rowMove = SafeInput.getRangedInt(…);
+        // int colMove = SafeInput.getRangedInt(…);
+
+    }
+}
